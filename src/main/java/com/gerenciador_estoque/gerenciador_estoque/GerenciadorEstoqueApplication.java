60b606ccea11f4e5659d1a7d33f3a2bd42a40937
Ulisses953 <ulisseskranzdamotta@gmail.com>
2023-06-02 17:54:22 -0300

    inicio do projeto

---
+++ src/main/java/com/gerenciador_estoque/gerenciador_estoque/GerenciadorEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciador_estoque.gerenciador_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciador_estoque/gerenciador_estoque/GerenciadorEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador_estoque.gerenciador_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
