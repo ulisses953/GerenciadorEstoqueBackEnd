465cf589fd3aba53a525e30717b6b0b0eb873757
ulisses953 <ulisseskranzdamotta@gmail.com>
2023-11-20 16:51:33 -0300

    desenvolviment do import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_category (id, name, description) VALUES (1,'Eletronicos', 'Eletronicos');
+INSERT INTO tb_category (id, name, description) VALUES (2,'Roupas', 'Roupas');
+INSERT INTO tb_category (id, name, description) VALUES (3,'Livros', 'Livros');
+INSERT INTO tb_category (id, name, description) VALUES (4,'Alimentos', 'Alimentos');
+INSERT INTO tb_category (id, name, description) VALUES (5,'Decoração', 'Decoração');
+INSERT INTO tb_category (id, name, description) VALUES (6,'Esportes', 'Esportes');
+INSERT INTO tb_category (id, name, description) VALUES (7,'Jogos', 'Jogos');
+INSERT INTO tb_category (id, name, description) VALUES (8,'Beleza', 'Beleza');
+INSERT INTO tb_category (id, name, description) VALUES (9,'Moveis', 'Moveis');
+INSERT INTO tb_category (id, name, description) VALUES (10,'Joias', 'Joias');
+
